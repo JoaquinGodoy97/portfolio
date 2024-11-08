bc9973a9a0c8516910e6a2ea0151aab25f591fbe
Joaquin Godoy <godoy.joaquin1997@gmail.com>
2024-11-08 14:19:57 -0300

    going for vercel deploy, netlify didnt work

--- tmp/scrapping/scrappingReadme.mjs
+++
@@ -1,9 +0,0 @@
-import { chromium } from 'playwright';
-
-export async function getRepoList() {
-    const browser = await chromium.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.goto('https://github.com/JoaquinGodoy97?tab=repositories');
-
-    const repoList = await page.$$eval()
-}

f5a751307426d8144ec78a0422ef9a7e138eaa95
Joaquin Godoy <godoy.joaquin1997@gmail.com>
2024-11-08 20:42:56 -0300

    testing

--- api/server.mjs
+++ api/server.mjs
@@ -1,3 +1,8 @@
 export default function handler(req, res) {
-    res.status(200).send({ message: 'Hello, World!' });
+    res.status(200).send({
+        "repoList": [
+            { "name": "Repo1", "url": "http://repo1.com" },
+            { "name": "Repo2", "url": "http://repo2.com" }
+        ]
+    });
 }
